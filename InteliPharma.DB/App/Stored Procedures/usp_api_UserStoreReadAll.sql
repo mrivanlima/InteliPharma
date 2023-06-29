bc3911cbb53ffb70c7700d7776faa32da6d92efe
Ryan Mirallia <ryanmirallia@gmail.com>
2023-06-29 11:35:12 -0300

    Stored Procedures add: "usp_api_UserStoreReadAll.sql" and "usp_api_UserStoreReadById.sql"

---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_UserStoreReadAll.sql
@@ -0,0 +1,13 @@
+﻿
+CREATE   PROCEDURE App.usp_api_UserStoreReadAll
+AS
+BEGIN 
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SELECT UserStoreId,
+		   UserId,
+		   StoreId,
+		   IsDefault
+	FROM App.UserStore;
+END;
