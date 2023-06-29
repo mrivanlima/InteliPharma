f344d161bccd99849e3aca115597baf3fbb6cb7d
Ryan Mirallia <ryanmirallia@gmail.com>
2023-06-28 23:07:09 -0300

    Stored Procedure add: ''usp_api_ZipCodeReadAll.sql''

---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_ZipCodeReadAll.sql
@@ -0,0 +1,14 @@
+﻿
+CREATE   PROCEDURE App.usp_api_ZipCodeReadAll
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SELECT	ZipCodeId,
+			ZipCodeCode,
+			StreetId,
+			ZipCodeFrom,
+			ZipCodeTo
+	FROM App.ZipCode;
+END;
