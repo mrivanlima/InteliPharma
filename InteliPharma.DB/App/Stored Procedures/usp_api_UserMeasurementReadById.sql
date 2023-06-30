9802fe0e4b99857e97ec7f87308b3efcb5482ef7
Ryan Mirallia <ryanmirallia@gmail.com>
2023-06-30 11:44:55 -0300

    Stored Procedures add: "usp_api_UserMeasurementReadById" and "usp_api_UserMeasurementUpdateById"

---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_UserMeasurementReadById.sql
@@ -0,0 +1,16 @@
+﻿
+CREATE  PROCEDURE App.usp_api_UserMeasurementReadById
+	@UserMeasurementId	INT
+AS 
+BEGIN
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SELECT	 UserMeasurementId,	
+			 UserId,			
+			 MeasurementId,	
+			 UserMeasurementDate
+	FROM App.UserMeasurement
+	WHERE UserMeasurementId = @UserMeasurementId;
+END;
+
