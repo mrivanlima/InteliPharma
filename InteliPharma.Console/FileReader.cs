269137af1b8660dd52a1ec38b59f8668307119bc
Ivan Lima <ivan.lima@talentei.com>
2023-06-30 12:59:00 -0400

    Add JSON for CEPS

---
+++ InteliPharma.Console/FileReader.cs
@@ -0,0 +1,57 @@
+﻿using InteliPharma.Console.Database;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace InteliPharma.Console
+{
+    public class FileReader
+    {
+
+
+
+        public void ReadFilesFromFolder (string path)
+        {
+            string connection = "Data Source=SQL8005.site4now.net;Initial Catalog=db_a9b211_intelipharma;User Id=db_a9b211_intelipharma_admin;Password=senha1234;";
+            var dbOptions = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlServer(connection).Options;
+            var dbContext = new ApplicationDbContext(dbOptions);
+
+            //if (dbContext != null)
+            //{
+            //    List<string> ceps = dbContext.cepGeoLocation.Select(e => e.postcode).ToList();
+            //    List<string> zips = dbContext.zipCodeInfo.Select(e => e.cep).ToList();
+            //    remaining = ceps.Except(zips).ToList();
+
+            //}
+            foreach (string file in Directory.EnumerateFiles(path, "*.json"))
+            {
+                string contents = File.ReadAllText(file);
+                CepInfo cepInfo = JsonSerializer.Deserialize<CepInfo>(contents);
+
+                ZipCodeInfo zipCodeInfo = new ZipCodeInfo();
+                zipCodeInfo.cep = cepInfo.cep;
+                zipCodeInfo.logradouro = cepInfo.logradouro;
+                zipCodeInfo.complemento = cepInfo.complemento;
+                zipCodeInfo.bairro = cepInfo.bairro;
+                zipCodeInfo.localidade = cepInfo.localidade;
+                zipCodeInfo.uf = cepInfo.uf;
+                zipCodeInfo.ibge = cepInfo.ibge;
+                zipCodeInfo.gia = cepInfo.gia;
+                zipCodeInfo.ddd = cepInfo.ddd;
+                zipCodeInfo.siafi = cepInfo.siafi;
+
+
+                if (dbContext != null)
+                {
+                    dbContext.zipCodeInfo.Add(zipCodeInfo);
+                    dbContext.SaveChanges();
+                }
+            }
+
+        }
+    }
+}
--- InteliPharma.Console/Program.cs
+++ InteliPharma.Console/Program.cs
@@ -6,32 +6,41 @@ using Microsoft.EntityFrameworkCore;
 using System.Security.Principal;
 using System.Text.Json;
 
-List<string> remaining = new  List<string>();
+
+string path = @"C:\Users\IvanLima\Documents\OpenCEP-main\v1";
+//FileReader files = new FileReader();
+//files.ReadFilesFromFolder(path);
 
 string connection = "Data Source=SQL8005.site4now.net;Initial Catalog=db_a9b211_intelipharma;User Id=db_a9b211_intelipharma_admin;Password=senha1234;";
 var dbOptions = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlServer(connection).Options;
 var dbContext = new ApplicationDbContext(dbOptions);
 
-if (dbContext != null)
-{
-    List<string> ceps = dbContext.cepGeoLocation.Select(e => e.postcode).ToList();
-    List<string> zips = dbContext.zipCodeInfo.Select(e => e.cep).ToList();
-   remaining = ceps.Except(zips).ToList();
-    
-}
+//if (dbContext != null)
+//{
+//    List<string> ceps = dbContext.cepGeoLocation.Select(e => e.postcode).ToList();
+//    List<string> zips = dbContext.zipCodeInfo.Select(e => e.cep).ToList();
+//    remaining = ceps.Except(zips).ToList();
 
-foreach(var r in remaining)
+//}
+
+List<string> zips = dbContext.zipCodeInfo.Select(e => e.cep).ToList();
+
+int count = 0;
+foreach (string file in Directory.EnumerateFiles(path, "*.json"))
 {
-    ApiCaller caller = new ApiCaller();
-    string cep = r.Remove(5, 1);
-    string result = caller.CallWebApi(cep);
-    Console.WriteLine(r);
-    if (!String.IsNullOrEmpty(result))
+    string contents = File.ReadAllText(file);
+    CepInfo cepInfo = JsonSerializer.Deserialize<CepInfo>(contents);
+
+    Console.WriteLine(cepInfo.cep +  " Counter " + ++count);
+    if (zips.Contains(cepInfo.cep))
     {
-        CepInfo cepInfo = JsonSerializer.Deserialize<CepInfo>(result);
-      
+        continue;
+    }
+
+    else
+    {
         ZipCodeInfo zipCodeInfo = new ZipCodeInfo();
-        zipCodeInfo.cep = r;
+        zipCodeInfo.cep = cepInfo.cep;
         zipCodeInfo.logradouro = cepInfo.logradouro;
         zipCodeInfo.complemento = cepInfo.complemento;
         zipCodeInfo.bairro = cepInfo.bairro;
@@ -43,27 +52,76 @@ foreach(var r in remaining)
         zipCodeInfo.siafi = cepInfo.siafi;
 
 
-        if (dbContext != null)
-        {
-            dbContext.zipCodeInfo.Add(zipCodeInfo);
-            dbContext.SaveChanges();
-        }
-    }
-    else
-    {
-        ZipCodeInfo zipCodeInfo = new ZipCodeInfo();
-        zipCodeInfo.cep = r;
-        
-
 
         if (dbContext != null)
         {
             dbContext.zipCodeInfo.Add(zipCodeInfo);
             dbContext.SaveChanges();
         }
-
     }
 }
+
+
+
+
+//List<string> remaining = new  List<string>();
+
+//string connection = "Data Source=SQL8005.site4now.net;Initial Catalog=db_a9b211_intelipharma;User Id=db_a9b211_intelipharma_admin;Password=senha1234;";
+//var dbOptions = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlServer(connection).Options;
+//var dbContext = new ApplicationDbContext(dbOptions);
+
+//if (dbContext != null)
+//{
+//    List<string> ceps = dbContext.cepGeoLocation.Select(e => e.postcode).ToList();
+//    List<string> zips = dbContext.zipCodeInfo.Select(e => e.cep).ToList();
+//   remaining = ceps.Except(zips).ToList();
+
+//}
+
+//foreach(var r in remaining)
+//{
+//    ApiCaller caller = new ApiCaller();
+//    string cep = r.Remove(5, 1);
+//    string result = caller.CallWebApi(cep);
+//    Console.WriteLine(r);
+//    if (!String.IsNullOrEmpty(result))
+//    {
+//        CepInfo cepInfo = JsonSerializer.Deserialize<CepInfo>(result);
+
+//        ZipCodeInfo zipCodeInfo = new ZipCodeInfo();
+//        zipCodeInfo.cep = r;
+//        zipCodeInfo.logradouro = cepInfo.logradouro;
+//        zipCodeInfo.complemento = cepInfo.complemento;
+//        zipCodeInfo.bairro = cepInfo.bairro;
+//        zipCodeInfo.localidade = cepInfo.localidade;
+//        zipCodeInfo.uf = cepInfo.uf;
+//        zipCodeInfo.ibge = cepInfo.ibge;
+//        zipCodeInfo.gia = cepInfo.gia;
+//        zipCodeInfo.ddd = cepInfo.ddd;
+//        zipCodeInfo.siafi = cepInfo.siafi;
+
+
+//        if (dbContext != null)
+//        {
+//            dbContext.zipCodeInfo.Add(zipCodeInfo);
+//            dbContext.SaveChanges();
+//        }
+//    }
+//    else
+//    {
+//        ZipCodeInfo zipCodeInfo = new ZipCodeInfo();
+//        zipCodeInfo.cep = r;
+
+
+
+//        if (dbContext != null)
+//        {
+//            dbContext.zipCodeInfo.Add(zipCodeInfo);
+//            dbContext.SaveChanges();
+//        }
+
+//    }
+//}
 
 
 
