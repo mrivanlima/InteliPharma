1dce776fe4ffb2be8201b9ce5f91784aacc5881e
Ivan Lima <ivan.lima@talentei.com>
2023-07-08 19:08:03 -0400

    Update DB

---
+++ InteliPharma.DB/Imp/Tables/FarmaciaPopular.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [Imp].[FarmaciaPopular] (
+    [medication] VARCHAR (MAX) NULL,
+    [usage]      VARCHAR (MAX) NULL,
+    [amount]     VARCHAR (MAX) NULL,
+    [state]      VARCHAR (MAX) NULL,
+    [value]      VARCHAR (MAX) NULL
+);
+
---
+++ InteliPharma.DB/Imp/Tables/FarmaciaPopularCoPay.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [Imp].[FarmaciaPopularCoPay] (
+    [Medication]  VARCHAR (MAX) NOT NULL,
+    [Usage]       VARCHAR (MAX) NOT NULL,
+    [Amount]      VARCHAR (MAX) NOT NULL,
+    [Referencia]  VARCHAR (MAX) NOT NULL,
+    [PrecoMaximo] VARCHAR (MAX) NOT NULL
+);
+
