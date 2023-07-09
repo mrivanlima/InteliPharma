c7cab269dafad4559a45b723063e2c7caea19caa
Ryan Mirallia <ryanmirallia@gmail.com>
2023-07-09 15:06:59 -0300

    ''Stored Procedure Add: usp_api_UserDiseaseUpdateById''

---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_UserDiseaseUpdateById.sql
@@ -0,0 +1,66 @@
+﻿
+CREATE   PROCEDURE App.usp_api_UserDiseaseUpdateById
+	@UserDiseaseId		INT,
+	@UserId				INT,
+	@DiseaseId			INT
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+	SET XACT_ABORT ON;
+
+	DECLARE @StoredProcedureName	VARCHAR(100) = 'usp_api_UserDiseaseUpdateById';
+	DECLARE @ErrorMessage			VARCHAR(100) = CONCAT('Error', ' ', @StoredProcedureName);
+	DECLARE @WarningMessage			VARCHAR(100);
+
+	IF NOT EXISTS (SELECT * FROM App.UserDisease WHERE UserDiseaseId = @UserDiseaseId)
+	BEGIN 
+		SET @ErrorMessage = CONCAT(@UserDiseaseId, ' ', 'not found!');
+		THROW 50005, @ErrorMessage, 1;
+	END
+
+	BEGIN TRY 
+		BEGIN TRANSACTION @StoredProcedureName
+
+			UPDATE u
+				SET UserId = @UserId,
+					DiseaseId = @DiseaseId
+			FROM App.UserDisease u
+			WHERE UserDiseaseId = @UserDiseaseId
+
+			PRINT CONCAT(@UserDiseaseId, ' ', 'updated sucessfully!');
+		COMMIT TRANSACTION @StoredProcedureName
+	END TRY
+
+	BEGIN CATCH
+
+		IF @@TRANCOUNT > 0
+		BEGIN
+			ROLLBACK TRANSACTION @StoredProcedureName;
+		END;
+
+		INSERT INTO [log].[ErrorLog] 
+		(
+			[ErrorNumber], 
+			[ErrorSeverity], 
+			[ErrorState], 
+			[ErrorProcedure], 
+			[ErrorLine], 
+			[ErrorMessage], 
+			[UserName],
+			ErrorDate
+		)
+		VALUES
+		( 
+			 ERROR_NUMBER()
+			,ERROR_SEVERITY()
+			,ERROR_STATE()
+			,ERROR_PROCEDURE()
+			,ERROR_LINE()
+			,ERROR_MESSAGE()
+			,SUSER_NAME()
+			,GETDATE()
+		);
+		PRINT @ErrorMessage;
+	END CATCH
+END;
