0dc12d54756bf5a82a2150c170ed59154f8835ed
Ryan Mirallia <ryanmirallia@gmail.com>
2023-07-10 12:37:45 -0300

    "usp_api_UserAddressReadAll.sql" and "usp_api_UserAddressReadById.sql" add

---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_UserAddressReadAll.sql
@@ -0,0 +1,14 @@
+﻿
+CREATE   PROCEDURE App.usp_api_UserAddressReadAll
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SELECT	UserAddressId,
+			AddressId,
+			UserId,
+			AddressTypeId,
+			Active
+	FROM App.UserAddress;
+END;
---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_UserAddressReadById.sql
@@ -0,0 +1,16 @@
+﻿
+CREATE   PROCEDURE App.usp_api_UserAddressReadById
+	@UserAddressId INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SELECT	UserAddressId,
+			AddressId,
+			UserId,
+			AddressTypeId,
+			Active
+	FROM App.UserAddress
+	WHERE UserAddressId = @UserAddressId;
+END;
