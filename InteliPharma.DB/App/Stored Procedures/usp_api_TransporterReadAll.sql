25e25f184a15bb1316eb8746901b1e4ba85dad7e
Ryan Mirallia <ryanmirallia@gmail.com>
2023-07-11 12:40:29 -0300

    "usp_api_TransporterReadAll.sql" and "usp_api_TransporterReadById.sql" add

---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_TransporterReadAll.sql
@@ -0,0 +1,15 @@
+﻿
+CREATE   PROCEDURE App.usp_api_TransporterReadAll
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SELECT	TransporterId,
+			AddressId,
+			TransporterName,
+			ANTTCode,
+			CNPJ,
+			StateSubscription
+	FROM App.Transporter;
+END;
---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_TransporterReadById.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE   PROCEDURE App.usp_api_TransporterReadById
+	@TransporterId SMALLINT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SELECT	TransporterId,
+			AddressId,
+			TransporterName,
+			ANTTCode,
+			CNPJ,
+			StateSubscription
+	FROM App.Transporter
+	WHERE TransporterId = @TransporterId;
+END;
