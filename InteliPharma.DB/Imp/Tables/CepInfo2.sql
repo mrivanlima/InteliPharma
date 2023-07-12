3a77a85e71f9015fbc21fafa44232c7ab9a3b612
Ivan Lima <ivan.lima@talentei.com>
2023-07-11 20:25:07 -0400

    Database addition

---
+++ InteliPharma.DB/Imp/Tables/CepInfo2.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [Imp].[CepInfo2] (
+    [CepId]       INT           IDENTITY (1, 1) NOT NULL,
+    [CidadeNome]  VARCHAR (MAX) NULL,
+    [ibge]        VARCHAR (MAX) NULL,
+    [ddd]         VARCHAR (MAX) NULL,
+    [estadoSigla] VARCHAR (MAX) NULL,
+    [Altitude]    VARCHAR (MAX) NULL,
+    [longitude]   VARCHAR (MAX) NULL,
+    [bairro]      VARCHAR (MAX) NULL,
+    [complemento] VARCHAR (MAX) NULL,
+    [cep]         VARCHAR (MAX) NULL,
+    [logradouro]  VARCHAR (MAX) NULL,
+    [latitude]    VARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([CepId] ASC)
+);
+
