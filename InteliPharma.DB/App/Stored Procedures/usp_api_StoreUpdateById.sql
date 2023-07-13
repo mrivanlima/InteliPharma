54b9855d4ced91daee949b0b6914ce2bedc7cd29
Ryan Mirallia <ryanmirallia@gmail.com>
2023-07-13 16:34:01 -0300

    "usp_api_StoreUpdateById.sql" add

---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_StoreUpdateById.sql
@@ -0,0 +1,68 @@
+﻿
+CREATE   PROCEDURE App.usp_api_StoreUpdateById
+	@StoreId		INT,
+    @StoreName		VARCHAR (200) NOT NULL,
+    @AddressId		INT           NOT NULL
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+	SET XACT_ABORT ON;
+
+	DECLARE @StoredProcedureName	VARCHAR(100) = 'usp_api_StoreUpdateById';
+	DECLARE @ErrorMessage			VARCHAR(100) = CONCAT('Error', ' ', @StoredProcedureName);
+	DECLARE @WarningMessage			VARCHAR(100);
+
+	SET @StoreName = TRIM(@StoreName);
+
+	IF NOT EXISTS (SELECT * FROM App.Store WHERE StoreId = @StoreId)
+	BEGIN
+		SET @ErrorMessage = CONCAT(@StoreId, ' ', 'not found!');
+		THROW 50005, @ErrorMessage, 1;
+	END
+	
+	BEGIN TRY
+		BEGIN TRANSACTION @StoredProcedureName
+
+			UPDATE s
+				SET		StoreName = @StoreName,
+						AddressId = @AddressId
+			FROM App.Store s
+			WHERE StoreId = @StoreId
+
+			PRINT CONCAT(@StoreName, ' ', 'updated successfully!');
+		COMMIT TRANSACTION @StoredProcedureName	
+	END TRY
+
+	BEGIN CATCH
+		
+		IF @@TRANCOUNT > 0
+		BEGIN
+			ROLLBACK TRANSACTION @StoredProcedureName;
+		END;
+
+		INSERT INTO [log].[ErrorLog] 
+		(
+			[ErrorNumber], 
+			[ErrorSeverity], 
+			[ErrorState], 
+			[ErrorProcedure], 
+			[ErrorLine], 
+			[ErrorMessage], 
+			[UserName],
+			ErrorDate
+		)
+		VALUES
+		( 
+			 ERROR_NUMBER()
+			,ERROR_SEVERITY()
+			,ERROR_STATE()
+			,ERROR_PROCEDURE()
+			,ERROR_LINE()
+			,ERROR_MESSAGE()
+			,SUSER_NAME()
+			,GETDATE()
+		);
+		PRINT @ErrorMessage;
+	END CATCH
+END;
