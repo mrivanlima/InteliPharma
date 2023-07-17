758b0dc52e7f11e67f142c646dca35de71a4b79a
Ryan Mirallia <ryanmirallia@gmail.com>
2023-07-17 15:52:05 -0300

    "usp_api_ShippingAgreementReadAll.sql" and "usp_api_ShippingAgreementReadById.sql" add

---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_ShippingAgreementReadAll.sql
@@ -0,0 +1,11 @@
+﻿
+CREATE   PROCEDURE App.usp_api_ShippingAgreementReadAll
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SELECT	ShippingAgreementId,
+			ShippingName
+	FROM App.ShippingAgreement;
+END;
---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_ShippingAgreementReadById.sql
@@ -0,0 +1,13 @@
+﻿
+CREATE   PROCEDURE App.usp_api_ShippingAgreementReadById
+	@ShippingAgreementId SMALLINT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SELECT	ShippingAgreementId,
+			ShippingName
+	FROM App.ShippingAgreement
+	WHERE ShippingAgreementId = @ShippingAgreementId;
+END;
