2f7a02ee3a72f56b8e80949d715087dc9c2f24fd
Ivan Lima <ivan.lima@talentei.com>
2023-07-17 08:28:41 -0400

    Cep

---
+++ InteliPharma.DB/stg/Tables/City.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [stg].[City] (
+    [CityId]        SMALLINT        IDENTITY (1, 1) NOT NULL,
+    [StateId]       TINYINT         NOT NULL,
+    [CityName]      VARCHAR (200)   NOT NULL,
+    [CityNameASCII] VARCHAR (200)   NOT NULL,
+    [Longitude]     DECIMAL (12, 9) NULL,
+    [Latitude]      DECIMAL (12, 9) NULL,
+    CONSTRAINT [PK_City] PRIMARY KEY CLUSTERED ([CityId] ASC)
+);
+
---
+++ InteliPharma.DB/stg/Tables/State.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [stg].[State] (
+    [StateId]           TINYINT         IDENTITY (1, 1) NOT NULL,
+    [StateName]         VARCHAR (20)    NOT NULL,
+    [StateNameASCII]    VARCHAR (20)    NOT NULL,
+    [StateAbbreviation] CHAR (2)        NOT NULL,
+    [Longitude]         DECIMAL (12, 9) NULL,
+    [Latitude]          DECIMAL (12, 9) NULL,
+    CONSTRAINT [PK_State] PRIMARY KEY CLUSTERED ([StateId] ASC)
+);
+
