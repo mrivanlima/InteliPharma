df79d947618d6dd1db6450bc2eab1e8ba5e71edc
Ryan Mirallia <ryanmirallia@gmail.com>
2023-07-17 13:37:13 -0300

    "usp_api_StockReadAll.sql" and "usp_api_StockReadById.sql" add

---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_StockReadAll.sql
@@ -0,0 +1,15 @@
+﻿
+CREATE   PROCEDURE App.usp_api_StockReadAll
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SELECT	StockId,
+			ProductId,
+			Lote,
+			FabricationDate,
+			ExpirationDate,
+			Quantity
+	FROM App.Stock;
+END;
---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_StockReadById.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE   PROCEDURE App.usp_api_StockReadById
+	@StockId BIGINT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SELECT	StockId,
+			ProductId,
+			Lote,
+			FabricationDate,
+			ExpirationDate,
+			Quantity
+	FROM App.Stock
+	WHERE StockId = @StockId;
+END;
