15612d17536bde4d8c25001b11f4caac891b51ef
Ryan Mirallia <ryanmirallia@gmail.com>
2023-07-18 17:48:34 -0300

    "usp_api_ProfessionalTypeReadAll.sql" and "usp_api_ProfessionalTypeReadById.sql" add

---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_ProfessionalTypeReadAll.sql
@@ -0,0 +1,11 @@
+﻿
+CREATE   PROCEDURE App.usp_api_ProfessionalTypeReadAll
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SELECT	ProfessionalTypeId,
+			ProfessionalTypeName
+	FROM App.ProfessionalType;
+END;
---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_ProfessionalTypeReadById.sql
@@ -0,0 +1,13 @@
+﻿
+CREATE   PROCEDURE App.usp_api_ProfessionalTypeReadById
+	@ProfessionalTypeId SMALLINT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SELECT	ProfessionalTypeId,
+			ProfessionalTypeName
+	FROM App.ProfessionalType
+	WHERE ProfessionalTypeId = @ProfessionalTypeId;
+END;
