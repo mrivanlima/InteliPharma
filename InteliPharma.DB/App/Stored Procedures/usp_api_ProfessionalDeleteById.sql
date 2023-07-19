2e3ed4c1575a1ed3e8c4cfc15cbd41a335fe9eed
Ryan Mirallia <ryanmirallia@gmail.com>
2023-07-19 14:45:09 -0300

    "usp_api_ProfessionalCreate.sql", "usp_api_ProfessionalDeleteById.sql", "usp_api_ProfessionalReadAll.sql", "usp_api_ProfessionalReadById.sql" and "usp_api_ProfessionalUpdateById.sql"  add

---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_ProfessionalDeleteById.sql
@@ -0,0 +1,63 @@
+﻿
+CREATE   PROCEDURE App.usp_api_ProfessionalDeleteById
+	@ProfessionalId	INT
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+	SET XACT_ABORT ON;
+
+	DECLARE @StoredProcedureName	VARCHAR(100) = 'usp_api_ProfessionalDeleteById';
+	DECLARE @ErrorMessage			VARCHAR(100) = CONCAT('Error', ' ', @StoredProcedureName);
+	DECLARE @WarningMessage			VARCHAR(100);
+
+
+	IF NOT EXISTS (SELECT * FROM App.Professional WHERE ProfessionalId = @ProfessionalId)
+	BEGIN
+		SET @ErrorMessage = CONCAT(@ProfessionalId, ' ', 'not found!');
+		THROW 50005, @ErrorMessage, 1;
+	END
+	
+	BEGIN TRY
+		BEGIN TRANSACTION @StoredProcedureName
+
+			DELETE p
+			FROM App.Professional p
+			WHERE ProfessionalId = @ProfessionalId
+
+			PRINT CONCAT(@ProfessionalId, ' ', 'Deleted successfully!');
+		COMMIT TRANSACTION @StoredProcedureName	
+	END TRY
+
+	BEGIN CATCH
+		
+		IF @@TRANCOUNT > 0
+		BEGIN
+			ROLLBACK TRANSACTION @StoredProcedureName;
+		END;
+
+		INSERT INTO [log].[ErrorLog] 
+		(
+			[ErrorNumber], 
+			[ErrorSeverity], 
+			[ErrorState], 
+			[ErrorProcedure], 
+			[ErrorLine], 
+			[ErrorMessage], 
+			[UserName],
+			ErrorDate
+		)
+		VALUES
+		( 
+			 ERROR_NUMBER()
+			,ERROR_SEVERITY()
+			,ERROR_STATE()
+			,ERROR_PROCEDURE()
+			,ERROR_LINE()
+			,ERROR_MESSAGE()
+			,SUSER_NAME()
+			,GETDATE()
+		);
+		PRINT @ErrorMessage;
+	END CATCH
+END;
---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_ProfessionalReadAll.sql
@@ -0,0 +1,12 @@
+﻿
+CREATE   PROCEDURE App.usp_api_ProfessionalReadAll
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SELECT		ProfessionalId,
+				ProfessionalName,
+				ProfessionalAssociationNumber
+	FROM App.Professional;
+END;
