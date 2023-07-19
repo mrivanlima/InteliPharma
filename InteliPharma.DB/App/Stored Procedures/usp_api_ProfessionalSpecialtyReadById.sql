3ede0fc7fd5ec9f627b3384189c9a39195cc1b4d
Ryan Mirallia <ryanmirallia@gmail.com>
2023-07-19 14:07:31 -0300

    "usp_api_ProfessionalSpecialtyReadById.sql" and "usp_api_ProfessionalSpecialtyReadAll.sql" add

---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_ProfessionalSpecialtyReadById.sql
@@ -0,0 +1,14 @@
+﻿
+CREATE   PROCEDURE App.usp_api_ProfessionalSpecialtyReadById
+	@ProfessionalSpecialtyId INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SELECT		ProfessionalSpecialtyId,
+				SpecialtyId,
+				ProfessionalId
+	FROM App.ProfessionalSpecialty
+	WHERE ProfessionalSpecialtyId = @ProfessionalSpecialtyId;
+END;
