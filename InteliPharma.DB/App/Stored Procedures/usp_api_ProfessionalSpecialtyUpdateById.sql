52f5a9320d4bb04caaf47a7130ae1b4e80ecc7b2
Ryan Mirallia <ryanmirallia@gmail.com>
2023-07-19 14:13:33 -0300

    "usp_api_ProfessionalSpecialtyUpdateById.sql" add

---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_ProfessionalSpecialtyUpdateById.sql
@@ -0,0 +1,69 @@
+﻿
+CREATE   PROCEDURE App.usp_api_ProfessionalSpecialtyUpdateById
+	@ProfessionalSpecialtyId		INT          NOT NULL,
+    @SpecialtyId					SMALLINT     NULL,
+    @ProfessionalId					VARCHAR (50) NULL
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+	SET XACT_ABORT ON;
+
+	DECLARE @StoredProcedureName	VARCHAR(100) = 'usp_api_ProfessionalSpecialtyUpdateById';
+	DECLARE @ErrorMessage			VARCHAR(100) = CONCAT('Error', ' ', @StoredProcedureName);
+	DECLARE @WarningMessage			VARCHAR(100);
+
+	
+
+	IF NOT EXISTS (SELECT * FROM App.ProfessionalSpecialty WHERE ProfessionalSpecialtyId = @ProfessionalSpecialtyId)
+	BEGIN
+		SET @ErrorMessage = CONCAT(@ProfessionalSpecialtyId, ' ', 'not found!');
+		THROW 50005, @ErrorMessage, 1;
+	END
+	
+	BEGIN TRY
+		BEGIN TRANSACTION @StoredProcedureName
+
+			UPDATE p
+				SET ProfessionalSpecialtyId = @ProfessionalSpecialtyId,
+					SpecialtyId = @SpecialtyId,
+					ProfessionalId = @ProfessionalId
+			FROM App.ProfessionalSpecialty p 
+			WHERE ProfessionalSpecialtyId = @ProfessionalSpecialtyId
+
+			PRINT CONCAT(@ProfessionalSpecialtyId, ' ', 'updated successfully!');
+		COMMIT TRANSACTION @StoredProcedureName	
+	END TRY
+
+	BEGIN CATCH
+		
+		IF @@TRANCOUNT > 0
+		BEGIN
+			ROLLBACK TRANSACTION @StoredProcedureName;
+		END;
+
+		INSERT INTO [log].[ErrorLog] 
+		(
+			[ErrorNumber], 
+			[ErrorSeverity], 
+			[ErrorState], 
+			[ErrorProcedure], 
+			[ErrorLine], 
+			[ErrorMessage], 
+			[UserName],
+			ErrorDate
+		)
+		VALUES
+		( 
+			 ERROR_NUMBER()
+			,ERROR_SEVERITY()
+			,ERROR_STATE()
+			,ERROR_PROCEDURE()
+			,ERROR_LINE()
+			,ERROR_MESSAGE()
+			,SUSER_NAME()
+			,GETDATE()
+		);
+		PRINT @ErrorMessage;
+	END CATCH
+END;
