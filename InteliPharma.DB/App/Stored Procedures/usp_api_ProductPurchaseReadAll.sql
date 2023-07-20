2368205006c7ad97054afc062d205b867a0fe3ee
Ryan Mirallia <ryanmirallia@gmail.com>
2023-07-20 16:38:24 -0300

    "ProductPurchaseReadById.sql" and "ProductPurchaseReadAll.sql"

---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_ProductPurchaseReadAll.sql
@@ -0,0 +1,29 @@
+﻿
+CREATE   PROCEDURE App.usp_api_ProductPurchaseReadAll
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SELECT		ProductPurchaseId,
+				ProductId,
+				PurchaseId,
+			    Quantity,
+				Price,
+				PurchaseDate,
+				Serial,
+				ProductBarCode,
+				ProductCode,
+				NCMSH,
+				CMCST,
+				CFOP,
+				UN,
+				UnitValue,
+				TotalValue,
+				ICMSBaseCalculation,
+				ICMSValue,
+				IPIValue,
+				ICMSPercent,
+				IPIPercent
+	FROM App.ProductPurchase;
+END;
---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_ProductPurchaseReadById.sql
@@ -0,0 +1,31 @@
+﻿
+CREATE   PROCEDURE App.usp_api_ProductPurchaseReadById
+	@ProductPurchaseId SMALLINT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SELECT		ProductPurchaseId,
+				ProductId,
+				PurchaseId,
+			    Quantity,
+				Price,
+				PurchaseDate,
+				Serial,
+				ProductBarCode,
+				ProductCode,
+				NCMSH,
+				CMCST,
+				CFOP,
+				UN,
+				UnitValue,
+				TotalValue,
+				ICMSBaseCalculation,
+				ICMSValue,
+				IPIValue,
+				ICMSPercent,
+				IPIPercent
+	FROM App.ProductPurchase
+	WHERE ProductPurchaseId = @ProductPurchaseId;
+END;
