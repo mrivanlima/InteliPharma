ff58ec00af2909e817202387414658d52b571abe
Ryan Mirallia <ryanmirallia@gmail.com>
2023-07-20 18:00:27 -0300

    "usp_api_ProductCartUserReadById.sql" and "usp_api_ProductCartUserReadAll.sql"

---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_ProductCartUserReadById.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE   PROCEDURE App.usp_api_ProductCartUserReadById
+	@ProductCartUserId SMALLINT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SELECT		ProductCartUserId,
+				ProductId,
+				CartId,
+				UserId,
+				AddedOn,
+				Active
+	FROM App.ProductCartUser
+	WHERE ProductCartUserId = @ProductCartUserId;
+END;
