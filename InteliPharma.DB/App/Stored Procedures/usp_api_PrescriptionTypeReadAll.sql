74e406da45219fea762dbdd84d4bfce04cd54df1
Ryan Mirallia <ryanmirallia@gmail.com>
2023-07-22 11:27:58 -0300

    "usp_api_PrescriptionTypeReadById.sql" and "usp_api_PrescriptionTypeReadAll.sql" add

---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_PrescriptionTypeReadAll.sql
@@ -0,0 +1,11 @@
+﻿
+CREATE   PROCEDURE App.usp_api_PrescriptionTypeReadAll
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SELECT	PrescriptionTypeId,
+			PrescriptionTypeName
+	FROM App.PrescriptionType;
+END;
---
+++ InteliPharma.DB/App/Stored Procedures/usp_api_PrescriptionTypeReadById.sql
@@ -0,0 +1,13 @@
+﻿
+CREATE PROCEDURE App.usp_api_PrescriptionTypeReadById
+	@PrescriptionTypeId TINYINT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SELECT	PrescriptionTypeId,
+			PrescriptionTypeName
+	FROM App.PrescriptionTypeId
+	WHERE PrescriptionTypeId = @PrescriptionTypeId;
+END;
