bfd68feaf2e1976e72498c59ab1199d1c354549a
Ryan Mirallia <ryanmirallia@gmail.com>
2023-08-19 09:18:36 -0300

    "IPrescriptionRepository.cs" and "PrescriptionRepository.cs" add

---
+++ InteliPharma.API/Services/IPrescriptionRepository.cs
@@ -0,0 +1,14 @@
+﻿using InteliPharma.Library.Entities;
+
+namespace InteliPharma.API.Services
+{
+    public interface IPrescriptionRepository
+    {
+        Task<Prescription> CreatePrescriptionAsync(Prescription prescription);
+        Task<IEnumerable<Prescription>> GetAllPrescriptionsAsync();
+        Task<Prescription> GetPrescriptionAsyncById(long prescriptionId);
+        Task<Prescription> UpdatePrescriptionAsyncById(Prescription prescription);
+        Task<bool> DeletePrescriptionAsyncById(long prescriptionId);
+
+    }
+}
