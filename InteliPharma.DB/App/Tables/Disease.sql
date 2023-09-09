594f5e65268d8041447ebf9fd7aac6f4592b7237
TheKauanLima <thekauanlima@gmail.com>
2023-09-08 20:27:14 -0400

    Fixed variable names in Disease.sql and Facility.sql

--- InteliPharma.DB/App/Tables/Disease.sql
+++ InteliPharma.DB/App/Tables/Disease.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [App].[Disease] (
     [DiseaseId]    INT          IDENTITY (1, 1) NOT NULL,
-    [DieseaseName] VARCHAR (50) NULL,
+    [DiseaseName] VARCHAR (50) NULL,
     CONSTRAINT [PK_Disease] PRIMARY KEY CLUSTERED ([DiseaseId] ASC)
 );
 
--- InteliPharma.DB/App/Tables/Facility.sql
+++ InteliPharma.DB/App/Tables/Facility.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [App].[Facility] (
     [FacilityId]   INT           IDENTITY (1, 1) NOT NULL,
-    [Facilityname] VARCHAR (100) NULL,
+    [FacilityName] VARCHAR (100) NULL,
     [AddressId]    INT           NULL,
     CONSTRAINT [PK_Facility] PRIMARY KEY CLUSTERED ([FacilityId] ASC)
 );
